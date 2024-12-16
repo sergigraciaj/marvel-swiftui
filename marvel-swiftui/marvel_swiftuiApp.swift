946ec80aa167694c13096b2e1cf2ea5aeda5406e
Sergi Gracia <sergiogj1990@gmail.com>
2024-12-16 18:36:27 +0100

    Add base files

---
+++ marvel-swiftui/marvel_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_swiftuiApp.swift
+//  marvel-swiftui
+//
+//  Created by Sergio Gracia Jimenez on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct marvel_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ marvel-swiftuiTests/marvel_swiftuiTests.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_swiftuiTests.swift
+//  marvel-swiftuiTests
+//
+//  Created by Sergio Gracia Jimenez on 16/12/24.
+//
+
+import Testing
+@testable import marvel_swiftui
+
+struct marvel_swiftuiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
